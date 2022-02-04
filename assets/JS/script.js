5d322ff9caf07d488262e0ba362750ce5bcc83a5
AngerOverApathy <angersavestheworld@gmail.com>
2022-02-03 23:30:30 -0800

    created local storage function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,28 +4,67 @@ var humidityText = $("#humidityText");
 var windText = $("windText");
 var uvText = $("#uvText");
 var forecastText = $("forecastText");
-var cardDisplay = $("#cardDisplay");
-var cardRows = $("#carardRows");
-var forecastDate = {};
-var forecastIcon = {};
-var forecastTemp = {};
-var forecastHum = {};
+
+var cities= [];
 
 var date = moment().format('DD' + "/" + 'MM' + '/' + 'YYYY');
 
+var myKey = "14be78c51cceb2d6a0448333fa09a1223";
 var url = "https://api.openweathermap.org/data/2.5/weather?q=";
-var APIKey = "14be78c51cceb2d6a0448333fa09a1223";
 
-var citiesArr = JSON.parse(localStorage.getItem("Saved City")) || [];
 
-$(document).ready(function (){
-    var userInput = citiesArray[citiesArray.length - 1];
-    currentWeather(userInput);
-    forecast(userInput);
-    lastSearch ();
+startSearch ();
 
-});
+function startSearch () {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+      if (storedCities !== null) {
+        cities = storedCities;
+      }
 
-function currentWeather () {
+    renderCities();
+
+};
+
+function saveCities(){
+  localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+function createCities() {
+    cityList.empty();
+
+      for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];
+        
+        var li = $("<li>").text(city);
+        li.attr("id","listC");
+        li.attr("data-city", city);
+        li.attr("class", "list-group-item");
+        cityList.prepend(li);
+      }
     
-}
+      if (!city){
+          return
+      } 
+      else{
+          getResponseWeather(city)
+      };
+  }   
+  
+    
+    $("#add-city").on("click", function(event){
+        event.preventDefault();
+  
+     //grab city from user input
+      var city = $("#city-input").val().trim();
+      
+      // if city is blank
+      if (city === "") {
+          return;
+      }
+      //city array input
+      cities.push(city);
+      // Store updated cities in localStorage, re-render the list
+    saveCities();
+    createCities();
+    });
+  
