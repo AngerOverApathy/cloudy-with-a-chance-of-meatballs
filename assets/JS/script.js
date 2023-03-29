490ec105f89151346164c015b9573f88fb49fb9d
AngerOverApathy <violasian@gmail.com>
2023-03-29 15:23:51 -0700

    reverse geocoding added

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,26 @@
 const button = document.getElementById('btn')
 
-button.addEventListener('click', searchCityWeather)
+button.addEventListener('click', getWeather)
 
-function searchCityWeather(){
-    const cityInput = document.getElementById('city-input').value
+
+function getWeather(){
+    const cityInput = document.getElementById('city-input').value.toLowerCase()
     console.log(cityInput)
+
+    // const state = document.getElementById('state-input').value.toLowerCase()
+    // console.log(state)
+
+    //get lat and lon
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput},US&limit=1&appid=f39fccfcbec15d0db53d6767eddf67f7`)
+            .then(res => res.json()) // parse response as JSON
+            .then(data => {
+            let lat = data[0].lat
+            let lon = data[0].lon
+            console.log(lat)
+            console.log(lon)
+            })
+            .catch(err => {
+                console.log(`error ${err}`)
+            });
+
 }
