0ff03c661f6ed307bbc089b301a28c61544272de
AngerOverApathy <violasian@gmail.com>
2023-03-29 15:43:20 -0700

    obtained lon and lat

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,12 @@
 const button = document.getElementById('btn')
+let latitude=''
+let longitude = ''
 
 button.addEventListener('click', getWeather)
 
 
 function getWeather(){
+
     const cityInput = document.getElementById('city-input').value.toLowerCase()
     console.log(cityInput)
 
@@ -14,10 +17,15 @@ function getWeather(){
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput},US&limit=1&appid=f39fccfcbec15d0db53d6767eddf67f7`)
             .then(res => res.json()) // parse response as JSON
             .then(data => {
-            let lat = data[0].lat
-            let lon = data[0].lon
-            console.log(lat)
-            console.log(lon)
+                latitude = data[0].lat
+                longitude = data[0].lon
+                
+                fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=f39fccfcbec15d0db53d6767eddf67f7`)
+                    .then(res => res.json()) // parse response as JSON
+                    .then(data => {
+                    console.log(data)
+            })
+            
             })
             .catch(err => {
                 console.log(`error ${err}`)
